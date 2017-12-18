47b9fd2c200d7aca29166291e1768361edc206c6
Jesús Iglesias <jesusgiglesias@gmail.com>
2017-12-18 21:56:52 +0100

    NEW (raspberrypi/utils.sh): Utilities for the main script

---
+++ raspberrypi/utils.sh
@@ -0,0 +1,93 @@
+#==========================================================================================================#
+#        PROJECT:           Hyot                                                                           #
+#           FILE:           utils.sh                                                                       #
+#                                                                                                          #
+#          USAGE:           ---                                                                            #
+#                                                                                                          #
+#    DESCRIPTION:           This script provides several utilities                                         #
+#                                                                                                          #
+#        OPTIONS:           ---                                                                            #
+#   REQUIREMENTS:           ---                                                                            #
+#          NOTES:           It must be loaded in the main script                                           #
+#         AUTHOR:           Jesús Iglesias García, jesus.iglesiasg@estudiante.uam.es                       #
+#   ORGANIZATION:           ---                                                                            #
+#        VERSION:           0.1                                                                            #
+#        CREATED:           12/18/17                                                                       #
+#       REVISION:           ---                                                                            #
+#==========================================================================================================#
+
+########################################
+#     COLOR AND MODE CAPABILITIES      #
+########################################
+
+# Modes
+bold=$(tput bold)                       # Set bold mode
+dim=$(tput dim)                         # Turn on half-bright mode
+underline_begin=$(tput smul)            # Begin underline mode
+underline_exit=$(tput rmul)             # Exit underline mode
+rev=$(tput rev)                         # Turn on reverse mode
+reset=$(tput sgr0)                      # Turn off all attributes
+
+# Colors
+black=$(tput setaf 0)
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+yellow=$(tput setaf 3)
+blue=$(tput setaf 4)
+magenta=$(tput setaf 5)
+cyan=$(tput setaf 6)
+white=$(tput setaf 7)
+
+########################################
+#              LOGGING                 #
+########################################
+
+# Header
+e_header () { 
+    printf "\n${bold}${blue}====================     %s     ====================${reset}\n" "$@" 
+}
+
+# Bold message
+e_message_bold () {
+     printf "\n${bold}${black}%s${reset}\n\n" "$@"
+}
+
+# Information message
+e_info () { 
+    printf "${cyan}➜ %s${reset}\n" "$@" 
+}
+
+# Success message
+e_success () { 
+    printf "${green}✔ %s${reset}\n" "$@"
+}
+
+# Error message
+e_error () { 
+    printf "${red}✖ %s${reset}\n" "$@"
+}
+
+# Warning message
+e_warning () { 
+    printf "${yellow}! %s${reset}\n" "$@"
+}
+
+# Underlined message
+e_underline () { 
+    printf "${underline}${bold}%s${reset}\n" "$@"
+}
+
+# Header in the 'Help' section
+help_header () { 
+    printf "\n${bold}${blue}==========  %s  ==========${reset}\n" "$@" 
+}
+
+# Bold and yellow message in the 'Help' section
+help_bold () { 
+    printf "${bold}${yellow}%s${reset}" "$@"
+}
+
+# Note TODO
+e_note () { 
+    printf "${underline}${bold}${blue2}Note:${reset}  ${blue2}%s${reset}\n" "$@"
+}
