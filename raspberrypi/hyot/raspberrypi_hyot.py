fa3084426a711a57e81c042a87363dc1c231da3d
Jesús Iglesias <jesusgiglesias@gmail.com>
2017-12-27 23:51:08 +0100

    NEW (raspberrypi/raspberrypi_hyot.py): File to receive data from sensors and send them to the Cloud

---
+++ raspberrypi/hyot/raspberrypi_hyot.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# coding: utf8
+#=======================================================================================================================#
+#                                                                                                                       #
+#                                    __    __   ___      ___   ________    __________                                   #
+#                                   |  |  |  |  \  \    /  /  |   __   |  |___    ___|                                  #
+#                                   |  |__|  |   \  \__/  /   |  |  |  |      |  |                                      #
+#                                   |   __   |    \_|  |_/    |  |  |  |      |  |                                      #
+#                                   |  |  |  |      |  |      |  |__|  |      |  |                                      #
+#                                   |__|  |__|      |__|      |________|      |__|                                      #
+#                                                                                                                       #
+#                                                                                                                       #
+#        PROJECT:     Hyot                                                                                              #
+#           FILE:     raspberrypi_hyot.py                                                                               #
+#                                                                                                                       #
+#          USAGE:     python raspberrypi_hyot.py                                                                        #
+#                                                                                                                       #
+#    DESCRIPTION:     This script XXX                                                                                   #
+#                                                                                                                       #
+#        OPTIONS:     ---                                                                                               #
+#   REQUIREMENTS:     Root user, Connected devices (16x2 LCD (2), DTH11 sensor, HC-SR04 sensor)                         #
+#          NOTES:     It must be run on a Raspberry Pi preferably with Raspbian as operating system                     #
+#         AUTHOR:     Jesús Iglesias García, jesus.iglesiasg@estudiante.uam.es                                          #
+#   ORGANIZATION:     ---                                                                                               #
+#        VERSION:     0.1                                                                                               #
+#        CREATED:     12/27/17                                                                                          #
+#       REVISION:     ---                                                                                               #
+#=======================================================================================================================#
