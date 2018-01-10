ef8c415bccbf17fca33634a9942f4722450dc11d
Jesús Iglesias <jesusgiglesias@gmail.com>
2018-01-10 23:34:39 +0100

    NEW (raspberrypi/dropbox_module.py): Module that contains the logic of Dropbox

---
+++ raspberrypi/hyot/dropbox_module.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# =====================================================================================================================#
+#                                                                                                                      #
+#                                    __    __   ___      ___   ________    __________                                  #
+#                                   |  |  |  |  \  \    /  /  |   __   |  |___    ___|                                 #
+#                                   |  |__|  |   \  \__/  /   |  |  |  |      |  |                                     #
+#                                   |   __   |    \_|  |_/    |  |  |  |      |  |                                     #
+#                                   |  |  |  |      |  |      |  |__|  |      |  |                                     #
+#                                   |__|  |__|      |__|      |________|      |__|                                     #
+#                                                                                                                      #
+#                                                                                                                      #
+#        PROJECT:     Hyot                                                                                             #
+#           FILE:     dropbox_module.py                                                                                #
+#                                                                                                                      #
+#          USAGE:     ---                                                                                              #
+#                                                                                                                      #
+#    DESCRIPTION:     This module contains the logic of the Dropbox service                                            #
+#                                                                                                                      #
+#        OPTIONS:     ---                                                                                              #
+#   REQUIREMENTS:     ---                                                                                              #
+#          NOTES:     It must be loaded by the main script: raspberrypi_hyot.py                                        #
+#         AUTHOR:     Jesús Iglesias García, jesus.iglesiasg@estudiante.uam.es                                         #
+#   ORGANIZATION:     ---                                                                                              #
+#        VERSION:     0.1                                                                                              #
+#        CREATED:     01/08/17                                                                                         #
+#       REVISION:     ---                                                                                              #
+# =====================================================================================================================#
+
+"""This module contains the logic of the Dropbox service"""
+
+########################################
+#               IMPORTS                #
+########################################
+
+try:
+    import sys                                          # System-specific parameters and functions
+    import time                                         # Time access and conversions
+    from colorama import Fore, Style                    # Cross-platform colored terminal text
+    import dropbox                                      # Python SDK for integrating with the Dropbox API v2
+
+except ImportError as importError:
+    print("Error to import: " + importError.message.lower() + ".")
+    sys.exit(1)
+except KeyboardInterrupt:
+    print("\rException: KeyboardInterrupt. Please, do not interrupt the execution.")
+    sys.exit(1)
+
