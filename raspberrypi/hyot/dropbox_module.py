102ccdfdadeb590720f971bbd3997fb1d825eb96
Jesús Iglesias <jesusgiglesias@gmail.com>
2018-01-10 23:35:56 +0100

    ADD (raspberrypi/dropbox_module.py): Connect and disconnect client

--- raspberrypi/hyot/dropbox_module.py
+++ raspberrypi/hyot/dropbox_module.py
@@ -46,3 +46,60 @@ except KeyboardInterrupt:
     print("\rException: KeyboardInterrupt. Please, do not interrupt the execution.")
     sys.exit(1)
 
+
+########################################
+#              CONSTANTS               #
+########################################
+TOKEN = "eI5UZqDlaNAAAAAAAAAAJm2xSwyCoMquSwWq7p270YXf5qr3p1vawOu5AzS99Uih"   # Authorisation token
+
+
+########################################
+#               FUNCTIONS              #
+########################################
+def connect():
+    """Creates an instance of the Dropbox class and establishes a connection"""
+
+    global dbx
+
+    print("      - Generating the client of the Dropbox service")
+
+    # Asks the user for Dropbox token
+    token = raw_input(Fore.BLUE + "        Enter the Dropbox token or empty to use the default value: "
+                      + Fore.RESET) or TOKEN
+
+    # Creates an instance of the Dropbox class, which can make requests to API
+    dbx = dropbox.Dropbox(token)
+
+    try:
+
+        # Checks that the access token is valid
+        current_user = dbx.users_get_current_account()
+
+        print(Fore.GREEN + "        Dropbox client connected with the user: " + current_user.name.given_name
+              + Fore.RESET)
+
+        time.sleep(1)
+
+    except dropbox.exceptions.AuthError as authError:
+
+        # Checks if the error is due to an invalid token
+        if authError.error.is_invalid_access_token():               # Access token has been revoked
+            print(Fore.RED + "        " + "The introduced access token is invalid because it has been revoked"
+                  + Fore.RESET)
+            sys.exit(1)
+        else:                                                       # Another error. For example: no write permission
+            raise
+
+
+def disconnect():
+    """Disconnects the Dropbox client disabling the access token used to authenticate the calls"""
+
+    global dbx
+
+    if not (dbx is None):
+        print("        Disconnecting the Dropbox client")
+
+        # Ends the client session
+        dbx = None
+
+        print(Fore.GREEN + "        Dropbox session ends successfully" + Fore.RESET)
