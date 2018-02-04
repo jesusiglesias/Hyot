b60e66c29de48c7c942b831c88360a4e2ee361d7
Jesús Iglesias <jesusgiglesias@gmail.com>
2018-02-04 17:37:13 +0100

    ADD (raspberrypi/gpg_module.py): Ask the user for the GPG directory and the password

--- raspberrypi/hyot/gpg_module.py
+++ raspberrypi/hyot/gpg_module.py
@@ -53,10 +53,10 @@ except KeyboardInterrupt:
 #              CONSTANTS               #
 ########################################
 # Path where GPG will store the public and private keyring files and a trust database
-HOMEDIR = os.path.dirname(os.path.abspath(__file__)) + "/gpg"
+GPGDIRDEFAULT = os.path.dirname(os.path.abspath(__file__)) + "/gpg"
 PUBKEY = "pub_hyot.gpg"                                                 # Public key
 SECKEY = "sec_hyot.gpg"                                                 # Secret key
-KEYS = HOMEDIR + "/hyot_keys.asc"                                       # File with the public and private keys
+KEYSFILE = "hyot_keys.asc"                                              # File with the public and private keys
 NAME = "Hyot"                                                           # Name
 EMAIL = "hyot.project@gmail.com"                                        # Email id
 PASS = "h7y1ot13"                                                       # Passphrase of private key
@@ -66,45 +66,94 @@ PASS = "h7y1ot13"                                                       # Passph
 #           GLOBAL VARIABLES           #
 ########################################
 gpg = None                                                              # GPG instance
+gpg_dir = None                                                          # GPG directory
 
 
 ########################################
 #               FUNCTIONS              #
 ########################################
+def generate_keys():
+    """"""
+
+    global gpg, gpg_dir, KEYSFILE, NAME, EMAIL, PASS
+
+    # Asks the user for the password of the private key
+    private_key_pass = raw_input(Fore.BLUE + "        Enter the password for the private key. Empty to use the default "
+                                             "value: " + Fore.RESET) or PASS
+
+    # Checks if the password is empty
+    if private_key_pass.isspace():
+        print(Fore.RED + "        The password can not be empty" + Fore.RESET)
+        sys.exit(1)
+
+    # Removes the spaces
+    private_key_pass = private_key_pass.replace(" ", "")
+
+    # Establishes the input data
+    input_data = gpg.gen_key_input(name_real=NAME, name_email=EMAIL, passphrase=private_key_pass)
+    key = gpg.gen_key(input_data)                                              # Creates the GPG key
+    keyid = str(key.fingerprint)                                               # Obtains the fingerprint
+
+    print(Fore.GREEN + "        GPG key created successfully with fingerprint: " + Style.BRIGHT + keyid
+          + Style.RESET_ALL)
+
+    public_key = gpg.export_keys(keyid)                                        # Exports the fingerprint (public key)
+    private_key = gpg.export_keys(keyid, True, passphrase=private_key_pass)    # Exports the fingerprint (private key)
+
+    # Stores the keys in a file
+    if public_key and private_key:
+
+        keys_path = gpg_dir + "/" + KEYSFILE                                   # Path where the keys file will be stored
+
+        with open(keys_path, 'w') as f:
+            f.write(public_key)
+            f.write(private_key)
+
+        print(Fore.GREEN + "        Public and private keys were stored in: " + Style.BRIGHT + keys_path
+              + Style.RESET_ALL)
+
+    else:
+        print(Fore.RED + "        Error to write the keys. Can't find key with fingerprint: " + keyid + Fore.RESET)
+        sys.exit(1)
+
+
 def init():
     """Creates the GPG instance and the public and private keys"""
 
-    global gpg, HOMEDIR, PUBKEY, SECKEY, KEYS, NAME, EMAIL, PASS
+    global gpg, gpg_dir, GPGDIRDEFAULT, PUBKEY, SECKEY, KEYSFILE
 
     try:
-
         print("\n      " + Style.BRIGHT + Fore.BLACK + "- Initializing GPG" + Style.RESET_ALL)
 
-        # Creates the GPG instance
-        gpg = gnupg.GPG(gnupghome=HOMEDIR, keyring=PUBKEY, secret_keyring=SECKEY)
+        # Asks the user for the GPG directory
+        gpg_dir = raw_input(Fore.BLUE + "        Enter the path of the directory used by GPG. Empty to use the default "
+                                        "value (" + GPGDIRDEFAULT + "): " + Fore.RESET) or GPGDIRDEFAULT
 
-        print(Fore.GREEN + "        Key rings and trust database were created in: " + Style.BRIGHT
-              + HOMEDIR + Style.RESET_ALL)
-
-        # Establishes the input data
-        input_data = gpg.gen_key_input(name_real=NAME, name_email=EMAIL, passphrase=PASS)
-        key = gpg.gen_key(input_data)                                       # Creates the keys
-        keyid = str(key.fingerprint)                                        # Obtains the fingerprint
-        public_key = gpg.export_keys(keyid)                                 # Exports the fingerprint (public key)
-        private_key = gpg.export_keys(keyid, True, passphrase=PASS)         # Exports the fingerprint (private key)
-
-        # Stores the keys in a file
-        if public_key and private_key:
-            with open(KEYS, 'w') as f:
-                    f.write(public_key)
-                    f.write(private_key)
-
-            print(Fore.GREEN + "        Public and private keys were stored in: " + Style.BRIGHT + KEYS + Style.RESET_ALL)
-        else:
-            print(Fore.RED + "        Error to write the keys. Can't find key with fingerprint: " + keyid + Fore.RESET)
+        # Checks if the path of the GPG directory is empty
+        if gpg_dir.isspace():
+            print(Fore.RED + "        The path of the GPG directory can not be empty" + Fore.RESET)
             sys.exit(1)
 
+        # Removes the spaces
+        gpg_dir = gpg_dir.replace(" ", "")
+
+        # Establishes the permissions only if the directory already exists
+        if os.path.exists(gpg_dir):
+            os.chmod(gpg_dir, 0700)
+
+        # Creates the GPG instance
+        gpg = gnupg.GPG(gnupghome=gpg_dir, keyring=PUBKEY, secret_keyring=SECKEY)
+
+        print(Fore.GREEN + "        Key rings and trust database were created in: " + Style.BRIGHT + gpg_dir
+              + Style.RESET_ALL)
+
+        # Generates the key
+        generate_keys()
+
         time.sleep(1)
+
+        print gpg.list_keys(False)
+        print gpg.list_keys(True)
 
     except Exception as initGPGError:
         print(Fore.RED + "        Error to initialize the GPG module: " + str(initGPGError) + Fore.RESET)
